70be042fdb45095ec9d317b8b5e49e3f3e9e6edf
sylathas <abate.niccolo@gmail.com>
2019-09-18 21:46:22 +0200

    Image done

--- sketch.js
+++ sketch.js
@@ -3,9 +3,20 @@ function preload(){
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
+
+  background(255);
+  frameRate(60);
 }
 
 function draw() {
-  // put drawing code here
+  var k = 100;
+  var r = random(120);
+  translate(width/2,height/2);
+  stroke(lerpColor(color("blue"), color("red"), r/120));
+  line(k, 0, k*cos(frameCount*3), k*sin(frameCount*3));
+  if(frameCount == 120) {
+    noLoop();
+  }
 }
