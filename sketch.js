d240325394099b0ef4f6b98272daa14ce3a4f798
sylathas <abate.niccolo@gmail.com>
2019-09-21 16:30:25 +0200

    Triple Sin()

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+var p = 0;
+var q = 0;
+var g = 0;
+
 function preload(){
   // put preload code here
 }
@@ -7,16 +11,66 @@ function setup() {
   angleMode(DEGREES);
 
   background(255);
-  frameRate(60);
+  frameRate(120);
 }
 
 function draw() {
-  var k = 100;
+
+  push();
+
+  var k = 50;
   var r = random(120);
-  translate(width/2,height/2);
-  stroke(lerpColor(color("blue"), color("red"), r/120));
-  line(k, 0, k*cos(frameCount*3), k*sin(frameCount*3));
-  if(frameCount == 120) {
+  p = p + 1;
+  q = q + 2;
+  g = g + 3;
+
+  //Originale
+
+  if(frameCount < 100){
+    translate(width/2,height/7);
+    stroke("black");
+    line(k, 0, (k)*cos(frameCount*4), (k)*sin(frameCount*4));
+  }
+
+  //Centro Triplo
+
+  pop();
+
+  translate(width/2,height/1.3);
+  stroke(lerpColor(color(0, random(255), random(255)), color(0, random(255), random(255)), r/120));
+  line(k, 0, k*cos(frameCount/2), k*sin(frameCount/2) + 30*sin(p));
+
+  stroke(lerpColor(color(random(255), 0, random(255)), color(random(255), 0, random(255)), r/120));
+  line(k, 0, k*cos(frameCount/2), k*sin(frameCount/2) + 30*sin(q));
+
+  stroke(lerpColor(color(random(255), random(255), 0), color(random(255), random(255), 0), r/120));
+  line(k, 0, k*cos(frameCount/2), k*sin(frameCount/2) + 30*sin(g));
+
+  //Sinistra Triplo
+
+  translate(-width/4,0);
+  stroke(lerpColor(color(0, random(255), random(255)), color(0, random(255), random(255)), r/120));
+  line(0, k, k*cos(frameCount/2), k*sin(frameCount/2) + 30*sin(p));
+
+  stroke(lerpColor(color(random(255), 0, random(255)), color(random(255), 0, random(255)), r/120));
+  line(0, k, k*cos(frameCount/2), k*sin(frameCount/2) + 30*sin(q));
+
+  stroke(lerpColor(color(random(255), random(255), 0), color(random(255), random(255), 0), r/120));
+  line(0, k, k*cos(frameCount/2), k*sin(frameCount/2) + 30*sin(g));
+
+  //Destra Triplo
+
+  translate(width/2,0);
+  stroke(lerpColor(color(random(255), random(255), 0), color(random(255), random(255), 0), r/120));
+  line(0, -k, k*cos(frameCount/2), k*sin(frameCount/2) + 30*sin(p));
+
+  stroke(lerpColor(color(random(255), 0, random(255)), color(random(255), 0, random(255)), r/120));
+  line(0, -k, k*cos(frameCount/2), k*sin(frameCount/2) + 30*sin(q));
+
+  stroke(lerpColor(color(0, random(255), random(255)), color(0, random(255), random(255)), r/120));
+  line(0, -k, k*cos(frameCount/2), k*sin(frameCount/2) + 30*sin(g));
+
+  if(frameCount == 720) {
     noLoop();
   }
 }
